9660366a9c1fc35652ad619bd429b9f2c3f25786
Angular CLI <angular-cli@angular.io>
2018-09-22 13:32:57 -0500

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('flask-socket-io-angular-chat-frontend App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
